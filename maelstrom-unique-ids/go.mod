d24caa2414b3b77b732f44643df387b9c8ccc28b
itoooak <101097771+itoooak@users.noreply.github.com>
2023-06-08 00:24:37 +0900

    Challenge 2: Unique ID Generation

---
+++ maelstrom-unique-ids/go.mod
@@ -0,0 +1,7 @@
+module maelstrom-unique-ids
+
+go 1.20
+
+require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20230516124010-52951329816e
+
+require github.com/rs/xid v1.5.0
---
+++ maelstrom-unique-ids/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "encoding/json"
+    "log"
+
+    maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	"github.com/rs/xid"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	n.Handle("generate", func (msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+		body["id"] = xid.New().String()
+
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
