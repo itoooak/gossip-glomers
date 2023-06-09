24bec1eefb3ec3448a5b2ce2bab37c13bc6652eb
itoooak <101097771+itoooak@users.noreply.github.com>
2023-06-09 16:22:34 +0900

    Challenge 3b: Multi-Node Broadcast

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -13,21 +13,40 @@ func main() {
 
 	values := struct {
 		sync.RWMutex
-		data []int
+		data map[int]struct{}
 	}{
 		sync.RWMutex{},
-		make([]int, 0),
+		make(map[int]struct{}),
 	}
 
-	n.Handle("broadcast", func (msg maelstrom.Message) error {
+	topology := struct {
+		sync.RWMutex
+		data map[string][]string
+	}{
+		sync.RWMutex{},
+		make(map[string][]string),
+	}
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
+		message := int(body["message"].(float64))
+
 		values.Lock()
-		values.data = append(values.data, int(body["message"].(float64)))
+		_, alreadyExists := values.data[message]
+		values.data[message] = struct{}{}
 		values.Unlock()
+
+		if !alreadyExists {
+			topology.RLock()
+			for _, neighbor := range topology.data[n.ID()] {
+				n.Send(neighbor, body)
+			}
+			topology.RUnlock()
+		}
 
 		repbody := make(map[string]any)
 		repbody["type"] = "broadcast_ok"
@@ -35,7 +54,7 @@ func main() {
 		return n.Reply(msg, repbody)
 	})
 
-	n.Handle("read", func (msg maelstrom.Message) error {
+	n.Handle("read", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
@@ -44,13 +63,17 @@ func main() {
 		repbody := make(map[string]any)
 		repbody["type"] = "read_ok"
 		values.RLock()
-		repbody["messages"] = values.data
+		messages := make([]int, 0)
+		for value := range values.data {
+			messages = append(messages, value)
+		}
+		repbody["messages"] = messages
 		values.RUnlock()
 
 		return n.Reply(msg, repbody)
 	})
 
-	n.Handle("topology", func (msg maelstrom.Message) error {
+	n.Handle("topology", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
@@ -58,6 +81,16 @@ func main() {
 
 		repbody := make(map[string]any)
 		repbody["type"] = "topology_ok"
+
+		topology.Lock()
+		for node, neighbors := range body["topology"].(map[string]interface{}) {
+			list := make([]string, 0)
+			for _, neighbor := range neighbors.([]interface{}) {
+				list = append(list, neighbor.(string))
+			}
+			topology.data[node] = list
+		}
+		topology.Unlock()
 
 		return n.Reply(msg, repbody)
 	})
